ddbe80a84d90602be06ac4f1d2b0479fd5ce6826
ck2mcv <ck2mcv@virginia.edu>
2026-03-11 15:41:42 -0400

    refactored lighting method

--- src/App/World/Environment.js
+++ src/App/World/Environment.js
@@ -158,355 +158,102 @@ export default class Environment {
   }
 
   addLights() {
-    // this.lightingFolder = this.pane.addFolder({ title: 'Lighting' });
-    // this.paperLantern = this.lightingFolder.addFolder({ title: 'Paper Lantern' });
+
+    const createPointLight = (color, intensity, distance, position, shadow = false) => {
+      const light = new THREE.PointLight(color, intensity, distance);
+      light.position.set(...position);
+
+      if (shadow) {
+        light.castShadow = true;
+        light.shadow.mapSize.set(1024, 1024);
+      }
+
+      this.scene.add(light);
+      return light;
+    } 
+
+    const createRectLight = (color, intensity, width, height, position, lookAt) => {
+      const light = new THREE.RectAreaLight(color, intensity, width, height);
+      light.position.set(...position);
+      light.lookAt(...lookAt);
+
+      this.scene.add(light);
+      return light;
+    }
+
+    // Global lighting
+    this.scene.add(new THREE.AmbientLight(0x111122, 9));
+    this.scene.add(new THREE.HemisphereLight(0x223344, 0x1a3a2a, 6));
     
-    // this.ambientLightFolder = this.lightingFolder.addFolder({ title: 'Ambient Light' });
-    const ambientLight = new THREE.AmbientLight(0x111122, 9);
-    // this.ambientLightFolder.addBinding(ambientLight, 'intensity', {
-    //   min: 0, 
-    //   max: 100, 
-    //   step: 1,
-    // });
-    this.scene.add(ambientLight);
+    // Ramen icon light
+    createPointLight(0xff00ff, 6, 8, [9, 17, 27], true);
 
-    // this.hemisphereLightFolder = this.lightingFolder.addFolder({ title: 'Hemisphere Light' });
-    const hemisphereLight = new THREE.HemisphereLight(0x223344, 0x1a3a2a, 6);
-    // this.hemisphereLightFolder.addBinding(hemisphereLight, 'intensity', {
-    //   min: 0, 
-    //   max: 100, 
-    //   step: 1,
-    // });
-    this.scene.add(hemisphereLight);
+    // Yakitori light
+    createPointLight(0xE7D400, 3, 8, [-3, 6.5, 25]);
+    
+    // Paper lanterns 
+    const lanternPositions = [
+      [-2.6, 10.3, 19.1],
+      [2, 12, 25],
+      [7.6, 12, 25],
+      [13, 12.2, 25]
+    ];
 
-    // this.ramenLightFolder = this.lightingFolder.addFolder({ title: 'RamenICON Light' });
-    const ramenLight = new THREE.PointLight(0xff00ff, 6, 8);
-    ramenLight.position.set(9, 17, 27);
-    ramenLight.castShadow = true;
-    ramenLight.shadow.mapSize.width = 1024;
-    ramenLight.shadow.mapSize.height = 1024;
-    this.scene.add(ramenLight);
+    lanternPositions.forEach(pos => {
+      createPointLight(0xE36012, 3, 8, pos);
+    });
 
-    // const sphereSize = 5; 
-    // const pointLightHelper = new THREE.PointLightHelper( ramenLight, sphereSize );
-    // this.scene.add(pointLightHelper);
+    // OPEN sign
+    createRectLight(0xDC0DE4, 4, 4, 2, [0.8, 13.2, 26.3], [0.8, 12.2, 31.3]);
 
-    // this.ramenLightFolder.addBinding(ramenLight, 'intensity', {
-    //   min: 0, 
-    //   max: 100, 
-    //   step: 1,
-    // });
+    // TV lights
+    const tvPositions = [
+      [-11.5, 25.1, 25.3],
+      [11.0, 25.1, 25.3],
+      [-3.0, 25.1, 25.3],  
+      [2.3, 25.1, 25.3]
+    ]
 
-    // this.yakitoriLightFolder = this.lightingFolder.addFolder({ title: 'Yakitori Light' });
-    const yakitoriLight = new THREE.PointLight(0xE7D400, 3, 8);
-    yakitoriLight.position.set(-3, 6.5, 25);
-    this.scene.add(yakitoriLight);
-
-    // const yakitoriLightHelper = new THREE.PointLightHelper( yakitoriLight, 2 );
-    // this.scene.add(yakitoriLightHelper);
-
-    // this.yakitoriLightFolder.addBinding(yakitoriLight, 'intensity', {
-    //   min: 0, 
-    //   max: 100, 
-    //   step: 1,
-    // });
-
-    const paperLanternLight1 = new THREE.PointLight(0xE36012, 3, 8);
-    paperLanternLight1.position.set(-2.6, 10.3, 19.1);
-    this.scene.add(paperLanternLight1);
-
-    // const paperLanternLightHelper = new THREE.PointLightHelper( paperLanternLight1, 3 );
-    // this.scene.add(paperLanternLightHelper);
-
-    // this.paperLantern.addBinding(paperLanternLight1, 'intensity', {
-    //   min: 0, 
-    //   max: 100, 
-    //   step: 0.1,
-    // });
-
-    const paperLanternLight2 = new THREE.PointLight(0xE36012, 3, 8);
-    paperLanternLight2.position.set(2, 12, 25);
-    this.scene.add(paperLanternLight2);
-
-    // const paperLanternLightHelper2 = new THREE.PointLightHelper( paperLanternLight2, 3 );
-    // this.scene.add(paperLanternLightHelper2);
-
-    // this.paperLantern.addBinding(paperLanternLight2, 'intensity', {
-    //   min: 0, 
-    //   max: 100, 
-    //   step: 1,
-    // });
-
-    const paperLanternLight3 = new THREE.PointLight(0xE36012, 3, 8);
-    paperLanternLight3.position.set(7.6, 12, 25);
-    this.scene.add(paperLanternLight3);
-
-    // const paperLanternLightHelper3 = new THREE.PointLightHelper( paperLanternLight3, 3 );
-    // this.scene.add(paperLanternLightHelper3);
-
-    // this.paperLantern.addBinding(paperLanternLight3, 'intensity', {
-    //   min: 0, 
-    //   max: 100, 
-    //   step: 0.1,
-    // });
-
-    const paperLanternLight4 = new THREE.PointLight(0xE36012, 3, 8);
-    paperLanternLight4.position.set(13, 12.2, 25);
-    this.scene.add(paperLanternLight4);
-
-    // const paperLanternLightHelper4 = new THREE.PointLightHelper( paperLanternLight4, 3 );
-    // this.scene.add(paperLanternLightHelper4);
-
-    // this.paperLantern.addBinding(paperLanternLight4, 'intensity', {
-    //   min: 0, 
-    //   max: 100, 
-    //   step: 0.1,
-    // });
-
-    // this.openSignLightFolder = this.lightingFolder.addFolder({ title: 'Open Sign Light' });
-
-    const openSignLight = new THREE.RectAreaLight(0xDC0DE4, 4, 4, 2);
-    const x = 0.8, y = 13.2, z = 26.3;
-    openSignLight.position.set(x, y, z);
-    openSignLight.lookAt(x, y - 1, z + 5);
-
-    // this.pane.addBinding(openSignLight, 'position', {
-    //   min: -100,
-    //   max: 100,
-    //   step: 0.1
-    // }).on('change', () => openSignLight.position.needsUpdate = true);
-
-    // const lookAtTarget = { x: 0, y: 0, z: 100 };
-    // this.pane.addBinding(lookAtTarget, 'x', {
-    //   min: -100, 
-    //   max: 100,
-    //   step: 0.1
-    // }).on('change', () => openSignLight.lookAt(lookAtTarget.x, lookAtTarget.y, lookAtTarget.z));
-    // this.pane.addBinding(lookAtTarget, 'y', {
-    //   min: -100, 
-    //   max: 100,
-    //   step: 0.1
-    // }).on('change', () => openSignLight.lookAt(lookAtTarget.x, lookAtTarget.y, lookAtTarget.z));
-    // this.pane.addBinding(lookAtTarget, 'z', {
-    //   min: -100, 
-    //   max: 100,
-    //   step: 0.1
-    // }).on('change', () => openSignLight.lookAt(lookAtTarget.x, lookAtTarget.y, lookAtTarget.z));
-
-    // this.openSignLightFolder.addBinding(openSignLight, 'intensity', {
-    //   min: 0, 
-    //   max: 100, 
-    //   step: 0.1,
-    // })
-
-    this.scene.add(openSignLight);
-    // const openSignLightHelper = new RectAreaLightHelper( openSignLight );
-    // openSignLight.add(openSignLightHelper);
-
-    // TV
-    // this.tvLightIntensity = this.lightingFolder.addFolder({ title: 'TV' });
-
-    const tvLight = new THREE.RectAreaLight(0xFFFFFF, 4, 8, 5);
-    tvLight.position.set(-11.5, 25.1, 25.3);
-    tvLight.lookAt(-11.5, 25.1 - 1, 25.3 + 5);
-    this.scene.add(tvLight);
-    // this.pane.addBinding(tvLight, 'position', {
-    //   min: -100, 
-    //   max: 100, 
-    //   step: 0.1,
-    // }).on('change', () => tvLight.position.needsUpdate = true);
-    // this.tvLightIntensity.addBinding(tvLight, 'intensity', {
-    //   min: 0,
-    //   max: 100, 
-    //   step: 0.1, 
-    // })
-    // const tvSignLightHelper = new RectAreaLightHelper( tvLight );
-    // tvLight.add( tvSignLightHelper ); 
-
-    const tvLight2 = new THREE.RectAreaLight(0xFFFFFF, 4, 8, 5);
-    tvLight2.position.set(11.0, 25.1, 25.3);
-    tvLight2.lookAt(11.0, 25.1 - 1, 25.3 + 5);
-    this.scene.add(tvLight2);
-    // this.tvLightIntensity.addBinding(tvLight2, 'intensity', {
-    //   min: 0,
-    //   max: 100, 
-    //   step: 0.1, 
-    // })
-    // const tvSignLightHelper2 = new RectAreaLightHelper( tvLight2 );
-    // tvLight2.add( tvSignLightHelper2 ); 
-
-    const tvLight3 = new THREE.RectAreaLight(0xFFFFFF, 4, 3.5, 5);
-    tvLight3.position.set(-3.0, 25.1, 25.3);
-    tvLight3.lookAt(-3.0, 25.1 - 1, 25.3 + 5);
-    this.scene.add(tvLight3);
-    // this.tvLightIntensity.addBinding(tvLight3, 'intensity', {
-    //   min: 0,
-    //   max: 100, 
-    //   step: 0.1, 
-    // })
-    // const tvSignLightHelper3 = new RectAreaLightHelper( tvLight3 );
-    // tvLight3.add( tvSignLightHelper3 ); 
-
-    const tvLight4 = new THREE.RectAreaLight(0xFFFFFF, 4, 3.5, 5);
-    tvLight4.position.set(2.3, 25.1, 25.3);
-    tvLight4.lookAt(2.3, 25.1 - 1, 25.3 + 5);
-    this.scene.add(tvLight4);
-    // this.tvLightIntensity.addBinding(tvLight4, 'intensity', {
-    //   min: 0,
-    //   max: 100, 
-    //   step: 0.1, 
-    // })
-    // const tvSignLightHelper4 = new RectAreaLightHelper( tvLight4 );
-    // tvLight4.add( tvSignLightHelper4 ); 
-
+    tvPositions.forEach(([x, y, z, w]) => {
+      createRectLight(
+        0xFFFFFF,
+        4,
+        w,
+        5,
+        [x, y, z],
+        [x, y - 1, z + 5]
+      );
+    });
+    
     // Akane
+    createRectLight(0xFFFFFF, 4, 4.5, 30, [24.7, 35.9, 18.2], [24.7, 34.9, 68.2]);
 
-    const akaneLight = new THREE.RectAreaLight(0xFFFFFF, 4, 4.5, 30);
-    akaneLight.position.set(24.7, 35.9, 18.2);
-    akaneLight.lookAt(24.7, 35.9 - 1, 18.2 + 50);
-    this.scene.add(akaneLight);
+    createRectLight(0xFFFFFF, 4, 4.5, 30, [24.7, 35.9, 16], [24.7, 34.9, -34]);
 
-    // this.akaneAkitsu = this.lightingFolder.addFolder({ title: 'AkaneAkitsu' });
-    // this.akaneAkitsu.addBinding(akaneLight, 'intensity', {
-    //   min: 0,
-    //   max: 100, 
-    //   step: 0.1, 
-    // })
-    // const akaneLightHelper = new RectAreaLightHelper( akaneLight );
-    // akaneLight.add( akaneLightHelper ); 
+    // KOU sign 
+    const kouPositions = [
+      [-22.2, 41.5, 29.2],
+      [-22.2, 37.7, 29.2],
+      [-22.2, 33.9, 29.2],
+      [-24.4, 41.5, 26.9],
+      [-24.4, 37.7, 26.9],
+      [-24.4, 33.9, 26.9]
+    ];
 
-    const akaneLight2 = new THREE.RectAreaLight(0xFFFFFF, 4, 4.5, 30);
-    akaneLight2.position.set(24.7, 35.9, 16);
-    akaneLight2.lookAt(24.7, 35.9 - 1, 16 - 50);
-    this.scene.add(akaneLight2);
+    kouPositions.forEach(pos => {
+      createPointLight(0xE10FE3, 5, 8, pos);
+    });
 
-    // this.akaneAkitsu.addBinding(akaneLight2, 'intensity', {
-    //   min: 0,
-    //   max: 100, 
-    //   step: 0.1, 
-    // })
-    // const akaneLightHelper2 = new RectAreaLightHelper( akaneLight2 );
-    // akaneLight2.add( akaneLightHelper2 ); 
+    // Asahi light
+    createRectLight(0xE70027, 4, 4, 2, [11.5, 12.1, 20], [11.5, 11.1, 25]);
 
-    // KOU Sign 
-    // this.KouSign = this.lightingFolder.addFolder({ title: 'KOU' });
+    // Ramen sign 
+    createRectLight(0x6C00FF, 4, 3.5, 5, [14.3, 8.3, 21.4], [21.8, 7.3, 21.4]);
 
-    const KfrontLight = new THREE.PointLight(0xE10FE3, 5, 8);
-    KfrontLight.position.set(-22.2, 41.5, 29.2);
-    this.scene.add(KfrontLight);
+    createRectLight(0x6C00FF, 4, 3.5, 5, [13.4, 8.3, 21.4], [5.9, 7.3, 21.4]);
 
-    // this.KouSign.addBinding(KfrontLight, 'intensity', {
-    //   min: 0,
-    //   max: 100, 
-    //   step: 0.1, 
-    // })
-
-    // const KfrontLightHelper = new THREE.PointLightHelper( KfrontLight, 3 );
-    // this.scene.add(KfrontLightHelper);
-
-    const OfrontLight = new THREE.PointLight(0xE10FE3, 5, 8);
-    OfrontLight.position.set(-22.2, 37.7, 29.2);
-    this.scene.add(OfrontLight);
-
-    // this.KouSign.addBinding(OfrontLight, 'intensity', {
-    //   min: 0,
-    //   max: 100, 
-    //   step: 0.1, 
-    // })
-
-    // const OfrontLightHelper = new THREE.PointLightHelper( OfrontLight, 3 );
-    // this.scene.add(OfrontLightHelper);
-
-    const UfrontLight = new THREE.PointLight(0xE10FE3, 5, 8);
-    UfrontLight.position.set(-22.2, 33.9, 29.2);
-    this.scene.add(UfrontLight);
-
-    // this.KouSign.addBinding(UfrontLight, 'intensity', {
-    //   min: 0,
-    //   max: 100, 
-    //   step: 0.1, 
-    // })
-
-    // const UfrontLightHelper = new THREE.PointLightHelper( UfrontLight, 3 );
-    // this.scene.add(UfrontLightHelper);
-
-    // KOU Sign 
-
-    const KBackLight = new THREE.PointLight(0xE10FE3, 5, 8);
-    KBackLight.position.set(-24.4, 41.5, 26.9);
-    this.scene.add(KBackLight);
-
-    // this.KouSign.addBinding(KBackLight, 'intensity', {
-    //   min: 0,
-    //   max: 100, 
-    //   step: 0.1, 
-    // })
-
-    // const KBackLightHelper = new THREE.PointLightHelper( KBackLight, 3 );
-    // this.scene.add(KBackLightHelper);
-
-    const OBackLight = new THREE.PointLight(0xE10FE3, 5, 8);
-    OBackLight.position.set(-24.4, 37.7, 26.9);
-    this.scene.add(OBackLight);
-
-    // this.KouSign.addBinding(OBackLight, 'intensity', {
-    //   min: 0,
-    //   max: 100, 
-    //   step: 0.1, 
-    // })
-
-    // const OBackLightHelper = new THREE.PointLightHelper( OBackLight, 3 );
-    // this.scene.add(OBackLightHelper);
-
-    const UBackLight = new THREE.PointLight(0xE10FE3, 5, 8);
-    UBackLight.position.set(-24.4, 33.9, 26.9);
-    this.scene.add(UBackLight);
-
-    // this.KouSign.addBinding(UBackLight, 'intensity', {
-    //   min: 0,
-    //   max: 100, 
-    //   step: 0.1, 
-    // })
-
-    // const UBackLightHelper = new THREE.PointLightHelper( UBackLight, 3 );
-    // this.scene.add(UBackLightHelper);
-
-    const asahiLight = new THREE.RectAreaLight(0xE70027, 4, 4, 2);
-    asahiLight.position.set(11.5, 12.1, 20);
-    asahiLight.lookAt(11.5, 12.1 - 1, 20 + 5);
-    this.scene.add(asahiLight);
-    // this.pane.addBinding(asahiLight, 'position', {
-    //   min: -100, 
-    //   max: 100, 
-    //   step: 0.1,
-    // }).on('change', () => asahiLight.position.needsUpdate = true);
-  
-    // const asahiLightHelper = new RectAreaLightHelper( asahiLight );
-    // asahiLight.add( asahiLightHelper ); 
-
-    const ramenRectLight = new THREE.RectAreaLight(0x6C00FF, 4, 3.5, 5);
-    ramenRectLight.position.set(14.3, 8.3, 21.4);
-    ramenRectLight.lookAt(14.3 + 7.5, 8.3 - 1, 21.4);
-    this.scene.add(ramenRectLight);
-  
-    // const ramenRectLightHelper = new RectAreaLightHelper( ramenRectLight );
-    // ramenRectLight.add( ramenRectLightHelper ); 
-
-    const ramenRectLight2 = new THREE.RectAreaLight(0x6C00FF, 4, 3.5, 5);
-    ramenRectLight2.position.set(13.4, 8.3, 21.4);
-    ramenRectLight2.lookAt(13.4 - 7.5, 8.3 - 1, 21.4);
-    this.scene.add(ramenRectLight2);
-  
-    // const ramenRectLightHelper2 = new RectAreaLightHelper( ramenRectLight2 );
-    // ramenRectLight2.add( ramenRectLightHelper2 ); 
-
-    const ramenArrowLight = new THREE.RectAreaLight(0xff00ff, 4, 3.5, 7);
-    ramenArrowLight.position.set(15.5, 15, 14.4);
-    ramenArrowLight.lookAt(15.5 + 7.5, 15 - 1, 14.4);
-    this.scene.add(ramenArrowLight);
-  
-    // const ramenArrowLightHelper2 = new RectAreaLightHelper( ramenArrowLight );
-    // ramenArrowLight.add( ramenArrowLightHelper2 ); 
+    // Ramen arrow
+    createRectLight(0xff00ff, 4, 3.5, 7, [15.5, 15, 14.4], [23, 14, 14.4]);
   }
 
   // addGround() {
